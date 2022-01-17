40b1dd5767a2319b22a2d8eeddf410235df4b395
alinashev <al.t.shevchenko@gmail.com>
2022-01-17 13:30:40 +0200

    changed setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from pip._internal.req import parse_requirements
 from setuptools import setup
 
-install = parse_requirements("requirements.txt")
-requirements = [str(ir.req) for ir in install]
 
 setup(
     name='Test',
     version='1.0',
     author='Alina Shevchenko',
     packages=['Calculate'],
-    install_requires=requirements,
+    install_requires=[
+        'numpy~=1.22.1',
+    ],
     scripts=[
         'main.py',
     ]
